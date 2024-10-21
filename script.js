59bf2e5fe763327f1ff7a24bc3c723f9f25580d0
Vladislav Miterev <miterew@mail.ru>
2024-10-22 00:00:10 +0300

    Решебник - №3 1/62

--- script.js
+++ script.js
@@ -1,18 +1,20 @@
 "use strict"
 
-const sideA = document.querySelector('#side');
-const sideB = document.querySelector('#side2');
+const r = document.querySelector('#Radius');
 const button = document.querySelector('button');
 
-button.addEventListener('click', function RectPS() {
+button.addEventListener('click', function calculateCircleArea() {
+    
+    // площадь окружности
     let s = document.createElement('p');
-    s.textContent = 'Площадь: ' + sideA.value * sideB.value;
+    let radius = parseFloat(r.value);
+    s.textContent = Math.PI * (radius ** 2);
     button.insertAdjacentElement('beforeBegin', s);
 
+    // длина окружности
+    let c = document.createElement('p');
+    c.textContent = 2 * Math.PI * radius;
+    button.insertAdjacentElement('beforeBegin', c);
 
-    let p = document.createElement('p');
-    p.textContent = 'Периметр: ' + (Number(sideA.value) + Number(sideB.value)) * 2;
-    button.insertAdjacentElement('beforeBegin', p);
-
-    button.removeEventListener('click', RectPS);
+    button.removeEventListener('click', calculateCircleArea);
 });
