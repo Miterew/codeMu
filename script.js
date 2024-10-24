506e6ad45182db9e47c5536471dc118f1eead209
Vladislav Miterev <miterew@mail.ru>
2024-10-24 23:14:34 +0300

    Решебник - #4 1/62

--- script.js
+++ script.js
@@ -1,20 +1,24 @@
 "use strict"
 
-const r = document.querySelector('#Radius');
+const side1 = document.querySelector('#side1');
+const side2 = document.querySelector('#side2');
+const side3 = document.querySelector('#side3');
 const button = document.querySelector('button');
 
-button.addEventListener('click', function calculateCircleArea() {
-    
-    // площадь окружности
-    let s = document.createElement('p');
-    let radius = parseFloat(r.value);
-    s.textContent = Math.PI * (radius ** 2);
-    button.insertAdjacentElement('beforeBegin', s);
+button.addEventListener('click', function calcS() {
+    let sideValue1 = Number(side1.value);
+    let sideValue2 = Number(side2.value);
+    let sideValue3 = Number(side3.value);
 
-    // длина окружности
-    let c = document.createElement('p');
-    c.textContent = 2 * Math.PI * radius;
-    button.insertAdjacentElement('beforeBegin', c);
 
-    button.removeEventListener('click', calculateCircleArea);
-});
+    let p = sideValue1 + sideValue2 + sideValue3;
+    let halfP = p / 2;
+
+    let geronFormula = Math.sqrt(halfP * ((halfP - sideValue1) * (halfP - sideValue2) * (halfP - sideValue3)));
+
+    let par = document.createElement('p');
+    par.textContent = geronFormula;
+    button.insertAdjacentElement('beforebegin', par);
+
+    button.removeEventListener('click', calcS);
+})
